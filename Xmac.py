0bcbc40d5dd73d538d56cdf3a08907ed41b508f7
kimanxo 313 <69109742+KIMANXO@users.noreply.github.com>
2021-04-23 17:10:57 +0100

    Xmac.py

--- Xmac.py
+++ Xmac.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
+import subprocess
+import optparse
+
+#Author = Kimanxo
 print(" ")
 print(" ")
 print("===[WELCOME TO Xmac CHANGER SCRIPT]===")
 print(" ")
-import subprocess
-import optparse
 
-def get_args():
+
+def get_args(): #a function to get options form the user 
 	parser = optparse.OptionParser()
 	parser.add_option("-i", "--interface", dest = "interface", help = "Interface To Change It's Mac Adress")
 	parser.add_option("-m", "--mac", dest = "mac", help = "The New  Mac Adress")
@@ -18,11 +21,11 @@ def get_args():
 		parser.error("please specify the interface and a new mac adress , type -h for more HELP")		
 	return options	
 
-def change_mac(interface, mac):
+def change_mac(interface, mac): #a function to change the Mac 
 	print('Changing The Mac Adress For ' + interface + ' to ' +  mac)
 	subprocess.call(["ifconfig", interface, "down"])
 	subprocess.call(["ifconfig", interface, "hw", "ether", mac])
 	subprocess.call(["ifconfig", interface, "up"])
 options = get_args()
-change_mac(options.interface, options.mac)
+change_mac(options.interface, options.mac) #excuting the function 
 
